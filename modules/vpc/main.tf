36c70deb34afb56a3293b38a2f48b58fba4012b4
Zubair Mazumder <zubairmazumder624@gmail.com>
2026-05-17 11:45:04 +0600

    add comment

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -121,6 +121,12 @@ resource "aws_nat_gateway" "nat" {
 
 #──────────────────────────────────────────────────────────────────────────────────
 #----------Public Route Table----------
+# Route Table = A set of rules that tells network traffic WHERE to go
+Route Table  =  Road signs in a city
+Routes       =  Individual signs ("To Airport → Turn Left")
+
+Without road signs → cars get lost, traffic goes nowhere
+With road signs    → traffic knows exactly where to go
 # Routes all internet traffic through IGW
 # Used by public subnet (bastion + NAT Gateway)
 resource "aws_route_table" "public" {
