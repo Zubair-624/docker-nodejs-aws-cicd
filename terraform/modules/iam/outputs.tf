c7c76398c9555125c8ad743f21d985efb3506351
Zubair Mazumder <zubairmazumder624@gmail.com>
2026-05-27 23:37:09 +0600

    add iam role outputs to use the ec2 module

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,13 +1,31 @@
-# IAM instance profile name → used by modules/ec2/main.tf
-# iam_instance_profile = module.iam.aws_iam_instance_profile_name
-output "aws_iam_instance_profile_name" {
-    description = "IAM instance profile name - used by ec2 module"
-    value       = aws_iam_instance_profile.ec2_profile.name
+# # IAM instance profile name → used by modules/ec2/main.tf
+# # iam_instance_profile = module.iam.aws_iam_instance_profile_name
+# output "aws_iam_instance_profile_name" {
+#     description = "IAM instance profile name - used by ec2 module"
+#     value       = aws_iam_instance_profile.ec2_profile.name
+# }
+
+# # IAM role name → used for attaching additional policies in future
+# # example: SSM policy attachment in future projects
+# output "iam_role_name" {
+#     description = "IAM role name - used for attaching additional policies"
+#     value       = aws_iam_role.ec2_role.name
+# }
+
+#==================================================================================
+# this output into my EC2 resource
+# ec2 resource → iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
+output "ssm_instance_profile_name" {
+
+    description = "Attach this to my EC2 instance resource"
+    value = aws_iam_instance_profile.ssm_profile.name
+  
 }
 
-# IAM role name → used for attaching additional policies in future
-# example: SSM policy attachment in future projects
-output "iam_role_name" {
-    description = "IAM role name - used for attaching additional policies"
-    value       = aws_iam_role.ec2_role.name
+output "ssm_role_arn" {
+
+    description = "ARN of the IAM role"
+    value = aws_iam_role.ssm_role.arn
+  
 }
+#==================================================================================
